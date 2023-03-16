0e7676f8c41a7c61c1f80cb15f41529ab17a2dad
PrettierFormatter <username@users.noreply.github.com>
2023-03-16 14:36:07 +0000

    Apply prettier formatted code

--- public/js/theme.js
+++ public/js/theme.js
@@ -8,16 +8,19 @@ function toggleTheme() {
 }
 
 function applyTheme() {
-    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
+    if (
+        localStorage.theme === 'dark' ||
+        (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
+    ) {
         document.querySelector('#theme-toggler .dark').classList.add('hidden');
         document.querySelector('#theme-toggler .light').classList.remove('hidden');
         document.body.classList.add('dark');
-        localStorage.theme = 'dark'
+        localStorage.theme = 'dark';
     } else {
         document.querySelector('#theme-toggler .dark').classList.remove('hidden');
         document.querySelector('#theme-toggler .light').classList.add('hidden');
         document.body.classList.remove('dark');
-        localStorage.theme = 'light'
+        localStorage.theme = 'light';
     }
 }
 
