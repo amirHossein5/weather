da764f8f4ece7525126e2590ca02e287dd7d9a44
amirHossein5 <amirHossein5@users.noreply.github.com>
2023-03-16 14:37:25 +0000

    deploy: d9ac85dc317a559202df47ec58250b250dc0e468

--- js/theme.js
+++ js/theme.js
@@ -8,14 +8,16 @@ function toggleTheme() {
 }
 
 function applyTheme() {
-    if (localStorage.theme === 'dark') {
+    if (localStorage.theme === 'dark' || (!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)) {
         document.querySelector('#theme-toggler .dark').classList.add('hidden');
         document.querySelector('#theme-toggler .light').classList.remove('hidden');
         document.body.classList.add('dark');
+        localStorage.theme = 'dark'
     } else {
         document.querySelector('#theme-toggler .dark').classList.remove('hidden');
         document.querySelector('#theme-toggler .light').classList.add('hidden');
         document.body.classList.remove('dark');
+        localStorage.theme = 'light'
     }
 }
 
