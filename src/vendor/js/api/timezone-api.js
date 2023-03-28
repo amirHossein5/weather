2876bc4ba011e7ce0416793c8f2dfa220047d5df
amirhossein5 <amir56665@yahoo.com>
2023-03-28 16:53:57 +0330

    Add timezone api file

---
+++ src/vendor/js/api/timezone-api.js
@@ -0,0 +1,14 @@
+import axios from 'axios'
+
+export async function getTimezoneOf({lat, long}) {
+    return axios
+        .get('https://api.open-meteo.com/v1/forecast?timezone=auto', {
+            params: {
+                latitude: lat,
+                longitude: long,
+            },
+        })
+        .then(({ data }) => {
+            return data.timezone;
+        });
+}
