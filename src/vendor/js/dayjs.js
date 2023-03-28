7cee875d8cf2d6beec7934a4dc7b9836a5e8076e
amirhossein5 <amir56665@yahoo.com>
2023-03-28 21:11:26 +0330

    Refactor dayjs file to set timezone dynamically

--- src/vendor/js/dayjs.js
+++ src/vendor/js/dayjs.js
@@ -4,8 +4,14 @@ import dayjsTz from 'dayjs/plugin/timezone';
 import dayjsUtc from 'dayjs/plugin/utc';
 
 dayjs.extend(isSameOrAfter);
-dayjs.extend(dayjsUtc);
-dayjs.extend(dayjsTz);
-dayjs.tz.setDefault(localStorage.timezone);
 
-export default dayjs;
+export function getInstace() {
+    return dayjs;
+}
+
+export function tz(timezone) {
+    dayjs.extend(dayjsUtc);
+    dayjs.extend(dayjsTz);
+    dayjs.tz.setDefault(timezone);
+    return dayjs.tz;
+}
